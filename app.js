fbaef811edbac1910794ab339b8e0cb1eac67bda
Asadbek Tursunaliyev <madrahimoffasadbek@gmail.com>
2025-04-30 09:27:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const $ = 
+const $$ = (es) => Array
+
+const productsWrap = $(".products")
+
+const products = [{
+    id:1,
+    image:"./skuter.png",
+    title: "Kugoo",
+    Battery: "2000 mAh",
+    speed: "60 km",
+    price: "1,2 ;.s",
+    powerHour: "5 hours",
+    discout: "29 900 rub",
+    type: "xit"
+}]
+
+const print = () => {
+    productsWrap.innerHTML
+}
