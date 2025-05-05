04b02a2b1f66f93c032f340fd991d7703bc36201
Asadbek Tursunaliyev <madrahimoffasadbek@gmail.com>
2025-05-05 08:49:32 +0500

    first commit

--- app.js
+++ app.js
@@ -59,58 +59,31 @@ const print = () => {
   productsWrap.innerHTML = "";
   products.map((p) => {
     productsWrap.innerHTML += `
-        <div data-id="${
-          p.id
-        }" class="w-full border rounded-xl overflow-hidden border-black/10">
+      <div data-id="${p.id}" class="w-full border rounded-xl overflow-hidden border-black/10">
         <div class="w-full bg-[#F0F1F5] aspect-square relative">
-          <img
-            src="${p.image}"
-            alt="${p.title}"
-            class="w-full object-contain h-ful"
-          />
-          <span
-            class="absolute top-2 left-2 inline-block px-2 py-0.5 rounded bg-[${
-              p.type == "Xit" ? "#EE685F" : "#75D14A"
-            }] text-sm text-white"
-            >${p.type}</span
-          >
+          <img src="${p.image}" alt="${p.title}" class="w-full object-contain h-ful" />
+          <span class="absolute top-2 left-2 inline-block px-2 py-0.5 rounded bg-[${p.type == "Xit" ? "#EE685F" : "#75D14A"}] text-sm text-white">${p.type}</span>
         </div>
         <div class="p-4">
-          <h3 class="text-lg font-[650]">Kugoo Kirin M4</h3>
+          <h3 class="text-lg font-[650]">${p.title}</h3>
           <ul class="text-black/60 grid grid-cols-2 gap-2 mt-3">
-            <li>
-              <i class="fa me-1 fa-car-battery"></i>
-              <span>${[p.battery]}</span>
-            </li>
-            <li>
-              <i class="fa me-1 fa-bolt"></i>
-              <span>${p.power}</span>
-            </li>
-            <li>
-              <i class="fa me-1 fa-gauge-high"></i>
-              <span>${p.speed}</span>
-            </li>
-            <li>
-              <i class="fa me-1 fa-stopwatch"></i>
-              <span>${p.powerHour}</span>
-            </li>
+            <li><i class="fa me-1 fa-car-battery"></i><span>${p.battery}</span></li>
+            <li><i class="fa me-1 fa-bolt"></i><span>${p.power}</span></li>
+            <li><i class="fa me-1 fa-gauge-high"></i><span>${p.speed}</span></li>
+            <li><i class="fa me-1 fa-stopwatch"></i><span>${p.powerHour}</span></li>
           </ul>
           <div class="flex justify-between mt-3 items-center">
             <div class="flex flex-col">
               ${p.discout ? `<del class="text-black/40">${p.price}</del>` : " "}
-              <span class="font-semibold text-2xl">${
-                p.discout ? p.discout : p.price
-              }</span>
+              <span class="font-semibold text-2xl">${p.discout}</span>
             </div>
-            <button data-id="${
-              p.id
-            }" class="addToCart w-14 border text-[#6F73EE] text-xl border-black/20 rounded-full aspect-square flex items-center justify-center transition-all hover:bg-[#6F73EE] hover:text-white cursor-pointer">
+            <button data-id="${p.id}" class="addToCart w-14 border text-[#6F73EE] text-xl border-black/20 rounded-full aspect-square flex items-center justify-center transition-all hover:bg-[#6F73EE] hover:text-white cursor-pointer">
               <i class="fa fas fa-shopping-basket select-none pointer-events-none"></i>
             </button>
           </div>
         </div>
       </div>
-      `;
+    `;
   });
 };
 
@@ -131,7 +104,7 @@ window.addEventListener("click", (e) => {
 
 print();
 
-let product = [];
+let product = JSON.parse(localStorage.getItem("products")) || [];
 
 document.getElementById("addBtn").addEventListener("click", function () {
   let title = document.getElementById("title").value;
@@ -142,15 +115,20 @@ document.getElementById("addBtn").addEventListener("click", function () {
   let powerHours = document.getElementById("powerHours").value;
 
   let newProduct = {
+    id: products.length + 1,
+    image: "./images/skuter.png",
     title,
     price,
     type,
     battery,
-    speed,
+    power,
     powerHours,
+    discout: price,
   };
 
-  product.push(newProduct);
+  products.push(newProduct);
+  localStorage.setItem("products", JSON.stringify(products));
+  print();
   showProduct();
 });
 
@@ -158,16 +136,14 @@ function showProduct() {
   let list = document.getElementById("product-list");
   list.innerHTML = "";
 
-  product.forEach(function (product, index) {
-    list.innerHTML += ` 
-      <div class="bg-white shadow-md rounded-lg p-4 space-y-2 border border-gray-200">
-        <h3 class="font-bold text-xl text-[#6F73EE]">Title: ${product.title}</h3>
-        <p class="text-gray-700"><span class="font-semibold">Price:</span> $${product.price}</p>
-        <p class="text-gray-700"><span class="font-semibold">Type:</span> ${product.type}</p>
-        <p class="text-gray-700"><span class="font-semibold">Battery:</span> ${product.battery} mAh</p>
-        <p class="text-gray-700"><span class="font-semibold">Speed:</span> ${product.speed} km/h</p>
-        <p class="text-gray-700"><span class="font-semibold">Power Hours:</span> ${product.powerHours} h</p>
-      </div><br>
+  products.forEach(function (product, index) {
+    list.innerHTML += `
+      
     `;
   });
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+  showProduct();
+  print();
+});
